6346969ce7da9efe4361dd7f88f9a284ff47943f
denizulku <deniz.uelkue@gmail.com>
2020-08-23 23:19:13 +0200

    Add animation

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ const game = () => {
     const options = document.querySelectorAll(".options button");
     const playerHand = document.querySelector(".player-hand");
     const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
+
+    hands.forEach((hand) => {
+      hand.addEventListener("animationend", function () {
+        this.style.animation = "";
+      });
+    });
     //Computer Options
 
     const computerOptions = ["rock", "paper", "scissors"];
@@ -30,11 +37,17 @@ const game = () => {
         const computerChoice = computerOptions[computerNumber];
 
         //Here is where we call compare hands
-        compareHands(this.textContent, computerChoice);
+        setTimeout(() => {
+          compareHands(this.textContent, computerChoice);
 
-        //Update images
-        playerHand.src = `./assets/${this.textContent}.png`;
-        computerHand.src = `./assets/${computerChoice}.png`;
+          //Update images
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 2000);
+
+        //Animation
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
       });
     });
   };
