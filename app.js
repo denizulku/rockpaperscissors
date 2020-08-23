ef9fa9e78d65f91dfc0b1e7ac79ffdeebd07e405
denizulku <deniz.uelkue@gmail.com>
2020-08-23 22:20:47 +0200

    Add Start Game

---
+++ app.js
@@ -0,0 +1,23 @@
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  //Start the game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+
+  //call all the innerfunction
+  startGame();
+};
+
+//start the game function
+
+game();
